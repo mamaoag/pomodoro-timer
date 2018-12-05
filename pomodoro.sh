9baccce7eb503afde48bc2a18c15668f55610c1e
Jio <jiomamaoag@gmail.com>
2018-12-05 22:20:19 +0800

    Modified the output of the terminal

--- pomodoro.sh
+++ pomodoro.sh
@@ -6,10 +6,20 @@ for REPEAT in $(eval echo {1..$1})
 do
 	echo "POMODORO # $REPEAT is starting"
 	sleep 1500 && notify-send "break"	
+	echo "POMODORO # $REPEAT has ended."
 	if (($REPEAT %4 != 0))
 	then
-		sleep 300 && notify-send "back to work"  # a short, 5-minute break
+		echo "5 Minute Break!"
+		sleep 300 && notify-send "Back to work"  # a short, 5-minute break
 	else
+		echo "15 Minute Break!"
 		sleep 900 && notify-send "back to work again"  # a long, 15-minute break
+	fi
+	
+	if (($REPEAT == $1))
+	then
+		echo "Your $REPEAT Pomodoros has been ended"
+	else
+		echo "Break if over! Get back to work!"
 	fi
 done
